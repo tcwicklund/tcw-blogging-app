6bca04218648a3ffa7f292c3804db68cef2dc8ae
Travis Wicklund <tcwicklund@gmail.com>
2020-06-02 23:46:33 -0400

    Refactored server to its own file

--- src/index.js
+++ src/index.js
@@ -1,30 +1,6 @@
 import '@babel/polyfill/noConflict'
-import { GraphQLServer, PubSub } from 'graphql-yoga'
-import db from './db'
+import server from './server'
 
-import { resolvers, fragmentReplacements } from './resolvers/index'
-import prisma from './prisma'
-
-const pubsub = new PubSub()
-
-const server = new GraphQLServer({
-    typeDefs: './src/schema.graphql',
-    resolvers: resolvers,
-    fragmentReplacements: fragmentReplacements,
-    context(request) {
-      return {
-        db,
-        pubsub,
-        prisma,
-        request
-      }
-    }
-})
-
-const opts = {
-  port: process.env.PORT
-}
-
-server.start(opts, () => {
+server.start(server.opts, () => {
     console.log(`Server is running`)
 })
---
+++ src/server.js
@@ -0,0 +1,26 @@
+import { GraphQLServer, PubSub } from 'graphql-yoga'
+import db from './db'
+
+import { resolvers, fragmentReplacements } from './resolvers/index'
+import prisma from './prisma'
+
+const pubsub = new PubSub()
+
+const server = new GraphQLServer({
+    typeDefs: './src/schema.graphql',
+    resolvers: resolvers,
+    fragmentReplacements: fragmentReplacements,
+    opts: {
+      port: process.env.PORT || 4000
+    },
+    context(request) {
+      return {
+        db,
+        pubsub,
+        prisma,
+        request
+      }
+    }
+})
+
+export {server as default}
